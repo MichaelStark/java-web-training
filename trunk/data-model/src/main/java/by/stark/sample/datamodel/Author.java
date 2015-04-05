2543623c43d44413b51bbc9beac3692308adb4c4
stark1912@gmail.com <stark1912@gmail.com@b1b46f0d-a22e-81a1-5b59-ce405a723811>
2015-04-05 00:41:29 +0000

    add for tests table-creators

--- trunk/data-model/src/main/java/by/stark/sample/datamodel/Author.java
+++ trunk/data-model/src/main/java/by/stark/sample/datamodel/Author.java
@@ -1,13 +1,7 @@
 package by.stark.sample.datamodel;
 
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 
 @Entity
 public class Author extends AbstractEntity {
@@ -18,9 +12,9 @@ public class Author extends AbstractEntity {
 	@Column
 	private String lastName;
 
-	@JoinTable(name = "book_2_author", joinColumns = { @JoinColumn(name = "author_id") }, inverseJoinColumns = { @JoinColumn(name = "book_id") })
+	/*@JoinTable(name = "book_2_author", joinColumns = { @JoinColumn(name = "author_id") }, inverseJoinColumns = { @JoinColumn(name = "book_id") })
 	@ManyToMany(targetEntity = Author.class, fetch = FetchType.LAZY)
-	private Set<Book> book;
+	private Set<Book> book;*/
 
 	public String getFirstName() {
 		return firstName;
--- trunk/data-model/src/main/java/by/stark/sample/datamodel/Genre.java
+++ trunk/data-model/src/main/java/by/stark/sample/datamodel/Genre.java
@@ -1,13 +1,7 @@
 package by.stark.sample.datamodel;
 
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 
 @Entity
 public class Genre extends AbstractEntity {
@@ -15,9 +9,9 @@ public class Genre extends AbstractEntity {
 	@Column
 	private String name;
 
-	@JoinTable(name = "book_2_genre", joinColumns = { @JoinColumn(name = "genre_id") }, inverseJoinColumns = { @JoinColumn(name = "book_id") })
+	/*@JoinTable(name = "book_2_genre", joinColumns = { @JoinColumn(name = "genre_id") }, inverseJoinColumns = { @JoinColumn(name = "book_id") })
 	@ManyToMany(targetEntity = Author.class, fetch = FetchType.LAZY)
-	private Set<Book> book;
+	private Set<Book> book;*/
 
 	public String getName() {
 		return name;
