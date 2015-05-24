b5bee0fe2b4eb51848f869dce32b14c7d7e749b0
stark1912@gmail.com <stark1912@gmail.com@b1b46f0d-a22e-81a1-5b59-ce405a723811>
2015-05-24 02:16:16 +0000

    pfffffffffffff

--- trunk/web-app/src/main/java/by/stark/sample/webapp/app/WicketWebApplication.java
+++ trunk/web-app/src/main/java/by/stark/sample/webapp/app/WicketWebApplication.java
@@ -14,8 +14,8 @@ import org.apache.wicket.util.lang.Bytes;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-import by.stark.sample.webapp.page.LoginPage;
 import by.stark.sample.webapp.page.home.book.BookPage;
+import by.stark.sample.webapp.page.login.LoginPage;
 
 @Component("wicketWebApplicationBean")
 public class WicketWebApplication extends AuthenticatedWebApplication {
